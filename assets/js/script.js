c77cd9c6dd7061f931179d0098f83221ba0532da
Arslan Majeed <arslanmajeed214@gmail.com>
2023-06-21 23:57:57 +0500

    Add some click events on the mobile menu item and fixed the blur effect.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,33 @@
 const targetDiv = document.getElementById("web-nav");
 const btn = document.getElementById("toggle");
 const cancel = document.getElementById("cross");
+const linkMenu = document.getElementsByClassName('menu-link');
 targetDiv.style.display = 'none';
 
 btn.onclick = function () {
   if (targetDiv.style.display !== "none") {
     targetDiv.style.display = "none";
+    document.body.style.overflowY = 'hidden';
   } else {
     targetDiv.style.display = "block";
+    document.body.style.overflowY = 'hidden';
   }
 };
 
 cancel.onclick = function () {
   if (targetDiv.style.display !== "none") {
     targetDiv.style.display = "none";
+    document.body.style.overflowY = 'scroll';
   } else {
     targetDiv.style.display = "block";
+    document.body.style.overflowY = 'scroll';
   }
+}
+
+for (let link of linkMenu) {
+  link.addEventListener('click', function() {
+    targetDiv.style.display = "none";
+    window.location.href = link.href;
+    document.body.style.overflowY = 'scroll';
+  });
 }
