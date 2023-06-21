287506924c469bd63785014ebc72a07d6b9ac906
mwovi <petermwovi73@gmail.com>
2023-06-21 20:42:16 +0300

    Add assets in assets folder

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const targetDiv = document.getElementById("web-nav");
+const btn = document.getElementById("toggle");
+const cancel = document.getElementById("cross");
+
+btn.onclick = function () {
+  if (targetDiv.style.display !== "none") {
+    targetDiv.style.display = "none";
+  } else {
+    targetDiv.style.display = "block";
+  }
+};
+
+cancel.onclick = function () {
+  if (targetDiv.style.display !== "none") {
+    targetDiv.style.display = "none";
+  } else {
+    targetDiv.style.display = "block";
+  }
+}
