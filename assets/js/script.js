69c7bb757a08149a1999d05ac81e4d94356c815c
kambereBr <brunokambere@gmail.com>
2023-06-21 22:39:07 +0200

    Setup ESLint and fix some javascript linters errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,36 @@
-const targetDiv = document.getElementById("web-nav");
-const btn = document.getElementById("toggle");
-const cancel = document.getElementById("cross");
+const targetDiv = document.getElementById('web-nav');
+const btn = document.getElementById('toggle');
+const cancel = document.getElementById('cross');
 const linkMenu = document.getElementsByClassName('menu-link');
 targetDiv.style.display = 'none';
 
-btn.onclick = function () {
-  if (targetDiv.style.display !== "none") {
-    targetDiv.style.display = "none";
+function showMenuAction() {
+  if (targetDiv.style.display !== 'none') {
+    targetDiv.style.display = 'none';
     document.body.style.overflowY = 'hidden';
   } else {
-    targetDiv.style.display = "block";
+    targetDiv.style.display = 'block';
     document.body.style.overflowY = 'hidden';
   }
-};
+}
 
-cancel.onclick = function () {
-  if (targetDiv.style.display !== "none") {
-    targetDiv.style.display = "none";
+function hideMenuAction() {
+  if (targetDiv.style.display !== 'none') {
+    targetDiv.style.display = 'none';
     document.body.style.overflowY = 'scroll';
   } else {
-    targetDiv.style.display = "block";
+    targetDiv.style.display = 'block';
     document.body.style.overflowY = 'scroll';
   }
 }
 
-for (let link of linkMenu) {
-  link.addEventListener('click', function() {
-    targetDiv.style.display = "none";
-    window.location.href = link.href;
+btn.addEventListener('click', showMenuAction);
+cancel.addEventListener('click', hideMenuAction);
+
+for (let i = 0; i < linkMenu.length; i += 1) {
+  linkMenu[i].addEventListener('click', () => {
+    targetDiv.style.display = 'none';
+    window.location.href = linkMenu[i].href;
     document.body.style.overflowY = 'scroll';
   });
 }
